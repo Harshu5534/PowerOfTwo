7bf7f86c844ca93c2886b897fd53725cd64bd82e
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-05-18 10:50:23 +0530

    [Harshal]Add:Power Of Two Task Is Done

---
+++ Worshop/PowerOfTwo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worshop
+{
+    public class PowerOfTwo
+    {
+        public void PowerTable()
+        {
+            int i = 0;
+            Console.WriteLine("Enter The Number");
+            int num=Convert.ToInt32(Console.ReadLine());
+            for(i=1;i<=num;i++)
+            {
+                Console.WriteLine("2" +" "+"^"+" "+i+"="+Math.Pow(2,i));
+            }
+        }
+    }
+}
---
+++ Worshop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Worshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PowerOfTwo two = new PowerOfTwo();
+            two.PowerTable();
+        }
+    }
+}
